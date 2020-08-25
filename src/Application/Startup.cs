69ab967059a46f83aec3d5b47a5aa3112bc4d85f
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-08-25 15:01:43 +0200

    add boilerplate

---
+++ src/Application/Startup.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Application
+{
+    public class Startup
+    {
+        private readonly Guid _fingerprint;
+    
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _fingerprint = Guid.NewGuid();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSpaStaticFiles(options => options.RootPath = "ClientApp/build");
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            // Sign each response with the fingerprint
+            app.Use((context, next) =>
+            {
+                context.Response.OnStarting(state =>
+                {
+                    context.Response.Headers.Add("Access-Control-Expose-Headers", "Api-Fingerprint");
+                    context.Response.Headers.Add("Api-Fingerprint", new[] {_fingerprint.ToString()});
+
+                    return Task.CompletedTask;
+                }, context);
+                return next.Invoke();
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            
+            app.UseSpaStaticFiles();
+            app.UseSpa(spa =>
+            {
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseReactDevelopmentServer("start");
+                }
+            });
+        }
+    }
+}
