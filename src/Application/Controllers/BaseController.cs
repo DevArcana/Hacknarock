da98c187d27f27c7417b12829cb848f507170db8
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-08-25 15:55:37 +0200

    add base controller

---
+++ src/Application/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers
+{
+    [Produces("application/json")]
+    [ApiController]
+    [Route("api/rest/[controller]")]
+    public abstract class BaseController : ControllerBase
+    {
+        
+    }
+}
--- src/Application/Controllers/WeatherForecastController.cs
+++ src/Application/Controllers/WeatherForecastController.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace Application.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class WeatherForecastController : BaseController
     {
         private static readonly string[] Summaries = new[]
         {
@@ -24,6 +23,7 @@ namespace Application.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         public IEnumerable<WeatherForecast> Get()
         {
             var rng = new Random();
