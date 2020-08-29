b7d01569abef47b337d2a1ea805c441f779f287b
Bartłomiej Chmiel <bachm44@gmail.com>
2020-08-29 17:46:46 +0200

    Removed auth

--- src/Application/ClientApp/src/App.js
+++ src/Application/ClientApp/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Switch } from "react-router-dom";
 import PrivateRoute from "./authentication/private-route";
 import { useAuth0 } from "@auth0/auth0-react";
-
+import { Route } from "react-router-dom";
+import { Login } from "./pages/Login/Login";
 import { Loading } from "./pages/Loading/Loading";
 import Error from "./pages/Error/Error";
-import Home from "./pages/Helper/Home/Home";
+import { Home as HelperHome } from "./pages/Helper/Home/Home";
 import MyHelpOffers from "./pages/Helper/MyHelpOffers/MyHelpOffers";
+import { Home as HelpeeHome } from "./pages/Helpee/Home/Home";
 
 function App() {
   const { isLoading, error, isAuthenticated } = useAuth0();
@@ -17,17 +19,33 @@ function App() {
   if (isLoading) {
     return <Loading />;
   }
-
   return (
     <Switch>
-      {/* <Route path="/login" component={Login} />*/}
+      <Route path="/login" component={Login} />
+      <PrivateRoute
+        path="/testAuth"
+        component={Error}
+        isAuthenticated={isAuthenticated}
+      />
+
+      {/*<PrivateRoute*/}
+      {/*  path="/help"*/}
+      {/*  component={MyHelpOffers}*/}
+      {/*  isAuthenticated={isAuthenticated}*/}
+      {/*/>*/}
+      {/*<PrivateRoute*/}
+      {/*  path="/addrequest"*/}
+      {/*  component={HelpeeHome}*/}
+      {/*  isAuthenticated={isAuthenticated}*/}
+      {/*/>*/}
       {/*<PrivateRoute*/}
       {/*  path="/"*/}
-      {/*  component={Home}*/}
-      {/*  isAuthenticated={isAuthenticated} */}
-      {/*/> */}
-      <PrivateRoute path="/myhelpoffers" component={MyHelpOffers} isAuthenticated={isAuthenticated} />
-      <PrivateRoute path="/" component={Home} isAuthenticated={isAuthenticated} />
+      {/*  component={HelperHome}*/}
+      {/*  isAuthenticated={isAuthenticated}*/}
+      {/*/>*/}
+      <Route path="/help" component={MyHelpOffers} />
+      <Route path="/addrequest" component={HelpeeHome} />
+      <Route path="/" component={HelperHome} />
     </Switch>
   );
 }
