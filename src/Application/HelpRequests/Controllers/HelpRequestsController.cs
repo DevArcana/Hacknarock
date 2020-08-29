d6c03e7af5589b18fc78850c6fe7d794c9c6bdab
Scytlee <Scytlee@disroot.org>
2020-08-29 13:30:26 +0200

    Add help requests controller and create list all endpoint

---
+++ src/Application/HelpRequests/Controllers/HelpRequestsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Controllers;
+using Application.HelpRequests.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.HelpRequests.Controllers
+{
+    public class HelpRequestsController : BaseController
+    {
+        private readonly IMediator _mediator;
+
+        public HelpRequestsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListHelpRequests([FromQuery] ListHelpRequestsQuery query)
+        {
+            try
+            {
+                return Ok(await _mediator.Send(query));
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
