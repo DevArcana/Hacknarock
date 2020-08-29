469024284edb1b40e57d31a55bad240a589b8cbe
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-08-29 10:50:00 +0200

    add tests controller

---
+++ src/Application/Controllers/TestsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Controllers
+{
+    [Authorize]
+    public class TestsController : BaseController
+    {
+        [HttpGet]
+        public ActionResult<string> GetSecretText()
+        {
+            return Ok("This is super secret text.");
+        }
+    }
+}
