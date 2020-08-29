c5ccf4acd8d32d4805724412611d39bd9791396d
Bartłomiej Chmiel <bachm44@gmail.com>
2020-08-29 21:36:11 +0200

    Configured navbars

---
+++ src/Application/ClientApp/src/pages/Helpee/MyHelpRequests/MyHelpRequests.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+const MyHelpRequests = (props) => <Typography>Your help requests</Typography>;
+
+export default MyHelpRequests;
