ddafb0eaa3a37b2c5259b9be06b566d4fc3ecbc4
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-08-29 13:07:14 +0200

    add command

---
+++ src/Application/HelpRequests/Commands/CreateHelpRequestCommand.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.HelpRequests.Models;
+using Application.Infrastructure.Persistance;
+using AutoMapper;
+using FluentValidation;
+using MediatR;
+
+namespace Application.HelpRequests.Commands
+{
+    public class CreateHelpRequestCommand : IRequest<HelpRequestDto>
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? Deadline { get; set; }
+        public int Urgency { get; set; }
+    }
+
+    public class CreateHelpRequestCommandValidator : AbstractValidator<CreateHelpRequestCommand>
+    {
+        public CreateHelpRequestCommandValidator()
+        {
+            RuleFor(x => x.Title)
+                .NotEmpty()
+                .MinimumLength(1)
+                .MaximumLength(100);
+
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .MinimumLength(1)
+                .MaximumLength(1000);
+
+            RuleFor(x => x.Urgency)
+                .GreaterThanOrEqualTo(0)
+                .LessThanOrEqualTo(3);
+        }
+    }
+
+    public class CreateHelpRequestCommandHandler : IRequestHandler<CreateHelpRequestCommand, HelpRequestDto>
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CreateHelpRequestCommandHandler(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        
+        public async Task<HelpRequestDto> Handle(CreateHelpRequestCommand request, CancellationToken cancellationToken)
+        {
+            var helpRequest = new HelpRequest(request.Title, request.Description, request.Deadline, request.Urgency);
+
+            _context.Add(helpRequest);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return _mapper.Map<HelpRequestDto>(helpRequest);
+        }
+    }
+}
---
+++ src/Application/HelpRequests/Models/HelpRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Application.Infrastructure.AutoMapper;
+
+namespace Application.HelpRequests.Models
+{
+    public class HelpRequestDto : IMapFrom<HelpRequest>
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? Deadline { get; set; }
+        public int Urgency { get; set; }
+    }
+}
