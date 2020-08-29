9d6f02c60149606033e397d0723c216b89d2edfa
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-08-30 00:34:41 +0200

    remove auth0 from home

--- src/Application/ClientApp/src/pages/Helper/Home/Home.jsx
+++ src/Application/ClientApp/src/pages/Helper/Home/Home.jsx
@@ -5,43 +5,21 @@ import { Typography } from "@material-ui/core";
 import Post from "./Post";
 import styled from "styled-components";
 import Box from "@material-ui/core/Box";
-import { useAuth0 } from "@auth0/auth0-react";
 import NavBar from "../NavBar/NavBar";
 
 export const Home = (props) => {
-  ///const [posts, setPosts] = useState([
-  //   {
-  //     title: "Piotr",
-  //     createdAt: "05.11 12:35",
-  //     description:
-  //       "Looking for some to do shopping for me!",
-  //   {
-  //     title: "Bartek",
-  //     createdAt: "05.11 12:35",
-  //     description:
-  //       "I am looking for someone to deliver a package to my friend.",
-  //   },
-  // ];
-
   const [posts, setPosts] = useState([]);
-  const { getAccessTokenWithPopup } = useAuth0();
 
   useEffect(() => {
     const fetchData = async () => {
-      const token = await getAccessTokenWithPopup({
-        audience: "http://devmountain-hacknarock.herokuapp.com/api/",
-      });
-      const result = await axios("/api/rest/requests", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+
+      const result = await axios("/api/rest/requests");
       console.log(result.data.results);
       setPosts(result.data.results);
     };
 
     fetchData();
-  }, [getAccessTokenWithPopup]);
+  }, []);
 
   return (
     <>
