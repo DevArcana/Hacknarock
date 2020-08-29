fe7905f05106176ca8c918b8f181ce5a3db64519
Scytlee <52714197+Scytlee@users.noreply.github.com>
2020-08-29 12:16:23 +0000

    Backend/helprequestscontroller (#4)
    * Add PaginateAsync to PaginationExtensions
    
    * Add help requests controller and create list all endpoint
    
    * Add create help request endpoint

---
+++ src/Application/HelpRequests/Controllers/HelpRequestsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Controllers;
+using Application.HelpRequests.Commands;
+using Application.HelpRequests.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.HelpRequests.Controllers
+{
+    public class HelpRequestsController : BaseController
+    {
+        private readonly IMediator _mediator;
+
+        public HelpRequestsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ListHelpRequests([FromQuery] ListHelpRequestsQuery query)
+        {
+            try
+            {
+                return Ok(await _mediator.Send(query));
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateHelpRequest([FromBody] CreateHelpRequestCommand command)
+        {
+            try
+            {
+                return Ok(await _mediator.Send(command));
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ src/Application/HelpRequests/Models/HelpRequestListDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Infrastructure.AutoMapper;
+
+namespace Application.HelpRequests.Models
+{
+    public class HelpRequestListDto : IMapFrom<HelpRequest>
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Urgency { get; set; }
+    }
+}
