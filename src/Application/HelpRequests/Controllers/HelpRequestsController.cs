c5e508e93ea9535a4f0e08ed404be936e7d35594
Scytlee <52714197+Scytlee@users.noreply.github.com>
2020-08-29 14:47:49 +0000

    Add get single, update and delete help request endpoints (#8)
    * Add get single, update and delete help request endpoints
    
    * Add SubmittedAt column to HelpRequests table
    
    * Modify endpoints to use SubmittedAt property
    
    * minor refactor
    
    * fix controller
    
    Co-authored-by: Piotr Krzystanek <piotr.krzystanek@gmail.com>

--- src/Application/HelpRequests/Controllers/HelpRequestsController.cs
+++ src/Application/HelpRequests/Controllers/HelpRequestsController.cs
@@ -11,6 +11,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Application.HelpRequests.Controllers
 {
+    [Route("api/rest/requests")]
     public class HelpRequestsController : BaseController
     {
         private readonly IMediator _mediator;
@@ -36,6 +37,51 @@ namespace Application.HelpRequests.Controllers
         [HttpPost]
         public async Task<IActionResult> CreateHelpRequest([FromBody] CreateHelpRequestCommand command)
         {
+            try
+            {
+                return Ok(await _mediator.Send(command));
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetHelpRequest(int id)
+        {
+            var query = new GetHelpRequestQuery(id);
+
+            try
+            {
+                return Ok(await _mediator.Send(query));
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateHelpRequest(int id, [FromBody] UpdateHelpRequestCommand command)
+        {
+            command.Id = id;
+
+            try
+            {
+                return Ok(await _mediator.Send(command));
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteHelpRequest(int id)
+        {
+            var command = new DeleteHelpRequestCommand(id);
+
             try
             {
                 return Ok(await _mediator.Send(command));
