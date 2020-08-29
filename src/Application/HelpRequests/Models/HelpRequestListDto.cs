355fafd9594374623165da7c75da54fb0054834b
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-08-29 15:40:55 +0200

    remove unneeded files

--- src/Application/HelpRequests/Models/HelpRequestListDto.cs
+++ src/Application/HelpRequests/Models/HelpRequestListDto.cs
@@ -9,8 +9,10 @@ namespace Application.HelpRequests.Models
     public class HelpRequestListDto : IMapFrom<HelpRequest>
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
         public int Urgency { get; set; }
+        public string Submitter { get; set; } = null!;
+        public DateTime SubmittedAt { get; set; }
     }
 }
