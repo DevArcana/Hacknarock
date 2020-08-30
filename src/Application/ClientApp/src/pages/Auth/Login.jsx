5621f91ebd7925403ca253ee45dd808849110751
Bartłomiej Chmiel <bachm44@gmail.com>
2020-08-30 03:17:23 +0200

    Unused vars

--- src/Application/ClientApp/src/pages/Auth/Login.jsx
+++ src/Application/ClientApp/src/pages/Auth/Login.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import Link from "@material-ui/core/Link";
 import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
