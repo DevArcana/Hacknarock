c40dedb85c1a82672c4c235859921357ce2bf3c2
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-08-30 11:27:36 +0200

    fix

--- src/Application/ClientApp/src/pages/Helpee/MyHelpRequests/HelpRequest.jsx
+++ src/Application/ClientApp/src/pages/Helpee/MyHelpRequests/HelpRequest.jsx
@@ -13,14 +13,10 @@ import moment from "moment";
 import { useState } from "react";
 import axios from "axios";
 import routes from "../../../routes";
-// import { useHistory } from "react-router-dom";
 
 const HelpRequest = (props) => {
   const { post } = props;
   const time = post.submittedAt;
-  // const history = useHistory();
-
-  const [isEditable, setIsEditable] = useState(false);
 
   const datePipe = () => {
     return <Moment format="MM/DD HH:mm">{moment(time).add(2, "hours")}</Moment>;
@@ -28,96 +24,44 @@ const HelpRequest = (props) => {
 
   const onDelete = async () => {
     await axios.delete(`${routes.api.requests}/${post.id}`);
-    // TODO: Reload list
     window.location.reload(false);
   }
 
-  const onSetEditable = () => {
-    setIsEditable(!isEditable);
-  }
-
-  const editable = () => {
-    return (
-      <CardContent style={{ wordWrap: "break-word" }}>
-        <SubtitlesBox>
-          <Typography color="textSecondary" variant="subtitle2">
-            {post.title}
-          </Typography>
-          <Typography>{datePipe()}</Typography>
-        </SubtitlesBox>
-        <Typography color="textSecondary">{post.description}</Typography>
-        <Box mb={3} />
-        {post.offers[0] !== undefined ? (
-          <ContactBox>
-            <Box>
-              <Typography variant="h5">{post.offers[0]?.firstName}</Typography>
-              <Typography>Wants to help you!</Typography>
-            </Box>
-            <PhoneBox clone>
-              <a href={"tel:" + post.offers[0]?.phoneNumber}>
-                <PhoneInTalkOutlinedIcon />
-              </a>
-
-              <PhoneInTalkOutlinedIcon />
-            </PhoneBox>
-          </ContactBox>
-        ) : (
-          <NotFoundBox>
-            <Typography color="textSecondary">
-              NOBODY WANTS TO HELP YOU
-            </Typography>
-          </NotFoundBox>
-        )}
-      </CardContent>
-    );
-  };
-
-  const notEditable = () => {
-    return (
-      <CardContent style={{ wordWrap: "break-word" }}>
-        <SubtitlesBox>
-          <Typography color="textSecondary" variant="subtitle2">
-            {post.title}
-          </Typography>
-          <Typography>{datePipe()}</Typography>
-        </SubtitlesBox>
-        <Typography color="textSecondary">{post.description}</Typography>
-        <Box mb={3} />
-        {post.offers[0] !== undefined ? (
-          <ContactBox>
-            <Box>
-              <Typography variant="h5">{post.offers[0]?.firstName}</Typography>
-              <Typography>Wants to help you!</Typography>
-            </Box>
-            <PhoneBox clone>
-              <a href={"tel:" + post.offers[0]?.phoneNumber}>
-                <PhoneInTalkOutlinedIcon />
-              </a>
-
-              <PhoneInTalkOutlinedIcon />
-            </PhoneBox>
-          </ContactBox>
-        ) : (
-          <NotFoundBox>
-            <Typography color="textSecondary">
-              NOBODY WANTS TO HELP YOU
-            </Typography>
-          </NotFoundBox>
-        )}
-      </CardContent>
-    );
-  };
-
   return (
     <PostCard>
-      {isEditable ? editable() : notEditable()}
+      <CardContent style={{ wordWrap: "break-word" }}>
+        <SubtitlesBox>
+          <Typography color="textSecondary" variant="subtitle2">
+            {post.title}
+          </Typography>
+          <Typography>{datePipe()}</Typography>
+        </SubtitlesBox>
+        <Typography color="textSecondary">{post.description}</Typography>
+        <Box mb={3} />
+        {post.offers[0] !== undefined ? (
+          <ContactBox>
+            <Box>
+              <Typography variant="h5">{post.offers[0]?.firstName}</Typography>
+              <Typography>Wants to help you!</Typography>
+            </Box>
+            <PhoneBox clone>
+              <a href={"tel:" + post.offers[0]?.phoneNumber}>
+                <PhoneInTalkOutlinedIcon />fix
+              </a>
+            </PhoneBox>
+          </ContactBox>
+        ) : (
+          <NotFoundBox>
+            <Typography color="textSecondary">
+              NOBODY WANTS TO HELP YOU
+            </Typography>
+          </NotFoundBox>
+        )}
+      </CardContent>
 
       <CardActions>
         <IconButton onClick={onDelete}>
           <DeleteOutlined />
-        </IconButton>
-        <IconButton onClick={onSetEditable}>
-          <EditOutlined />
         </IconButton>
       </CardActions>
     </PostCard>
